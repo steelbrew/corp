7325770b051c8ace0a2bba5d0b33f6a0b67140f7
Thomas Fauskanger <thomasfauskanger@gmail.com>
2020-02-23 22:38:25 +0100

    Updates

---
+++ precache-manifest.72acb3733860dc33d46c372c74e23a68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2949d9120cbf28702752af42f4654734",
+    "url": "/steelbrew/index.html"
+  },
+  {
+    "revision": "f747b69e970f8b689bb7",
+    "url": "/steelbrew/static/js/2.b4054e73.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/steelbrew/static/js/2.b4054e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1531af8579f616ac442",
+    "url": "/steelbrew/static/js/main.98d2dbd0.chunk.js"
+  },
+  {
+    "revision": "250ce736616f52a9850d",
+    "url": "/steelbrew/static/js/runtime-main.ffd2a5e6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3f935302f369e2ea01bb65e5f8b9ea3b.js"
+  "/steelbrew/precache-manifest.72acb3733860dc33d46c372c74e23a68.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/steelbrew/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
